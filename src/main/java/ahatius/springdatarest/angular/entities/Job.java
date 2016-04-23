7d306d21c4bb49c9193a0e5afdb1dcc50835418f
ahatius <plsnospamthx@gmail.com>
2016-04-23 12:47:43 +0200

    Remove circular dependencies for Job and JobDetail

--- src/main/java/ahatius/springdatarest/angular/entities/Job.java
+++ src/main/java/ahatius/springdatarest/angular/entities/Job.java
@@ -1,7 +1,5 @@
 package ahatius.springdatarest.angular.entities;
 
-import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,7 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 /**
  * Represents a job
@@ -19,57 +16,41 @@ import javax.persistence.OneToMany;
  */
 @Entity
 public class Job {
-  @Id
-  @GeneratedValue
-  private int jobId;
+	@Id
+	@GeneratedValue
+	private int jobId;
 
-  @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
-  @JoinColumn(name = "customerId")
-  private Customer customer;
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+	@JoinColumn(name = "customerId")
+	private Customer customer;
 
-  @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "job",
-      orphanRemoval = true)
-  private List<JobDetail> jobDetails;
+	/**
+	 * @return the jobId
+	 */
+	public int getJobId() {
+		return jobId;
+	}
 
-  /**
-   * @return the jobId
-   */
-  public int getJobId() {
-    return jobId;
-  }
+	/**
+	 * @param jobId
+	 *            the jobId to set
+	 */
+	public void setJobId(int jobId) {
+		this.jobId = jobId;
+	}
 
-  /**
-   * @param jobId the jobId to set
-   */
-  public void setJobId(int jobId) {
-    this.jobId = jobId;
-  }
+	/**
+	 * @return the customer
+	 */
+	public Customer getCustomer() {
+		return customer;
+	}
 
-  /**
-   * @return the customer
-   */
-  public Customer getCustomer() {
-    return customer;
-  }
-
-  /**
-   * @param customer the customer to set
-   */
-  public void setCustomer(Customer customer) {
-    this.customer = customer;
-  }
-
-  /**
-   * @return the jobDetails
-   */
-  public List<JobDetail> getJobDetails() {
-    return jobDetails;
-  }
-
-  /**
-   * @param jobDetails the jobDetails to set
-   */
-  public void setJobDetails(List<JobDetail> jobDetails) {
-    this.jobDetails = jobDetails;
-  }
+	/**
+	 * @param customer
+	 *            the customer to set
+	 */
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
 }
